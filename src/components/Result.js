ddc361d0aaf6251f16ad8bdb1d883fc2e3d6e85e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-01 19:03:59 +1100

    Update Result component with api data

--- src/components/Result.js
+++ src/components/Result.js
@@ -10,8 +10,22 @@ const Result = ({
       }
     </div>
     <div>
+      {
+        !!data && data.results.map( result => (
+          <div>
+            { result.partOfSpeech }<br/>
+            { result.definition }<br/>
+            { !!result.synonyms && result.synonyms.join(', ') }<br/>
+            { !!result.similarTo && result.similarTo.join(', ') }<br/>
+            { !!result.derivation && result.derivation.join(', ') }<br/>
+            { !!result.examples && result.examples.join(', ') }<br/>
+          <hr/>
+          </div>
+        ))
+      }
     </div>
-    <div>
+    
+    <div><hr/>
       {JSON.stringify(data)}
     </div>
   </div>

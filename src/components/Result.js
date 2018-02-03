db6de8495a7903bebfb2d338e23a2479aa444f5e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-03 11:52:12 +1100

    Update validations

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,66 +1,77 @@
 import React from 'react'
 
-const Result = ({ data, error }) => (
+const Result = ({ word, data, error }) => (
   <div className="container mt-3">
     {!error ? (
       !!data ? (
-      <div>
-        {!!data.syllables && (
-        <h2>
-          {// Syllables
-          data.syllables.list.join('-')}
-        </h2>
-        )}
-        {!!data.pronunciation && (
-          <p className="font-italic pronunciation">
-            {// Pronunciation
-            '\\' + data.pronunciation.all + '\\'}
-          </p>
-        )}
         <div>
-          {!!data.results ? (
-            data.results.map(result => (
-              <div>
-                <p className="font-italic partOfSpeech">
-                  {result.partOfSpeech}
-                </p>
-                <p>{': ' + result.definition}</p>
-                {!!result.synonyms && (
-                  <p>
-                    <strong>synonyms</strong>
-                    <p>{result.synonyms.join(', ')}</p>
-                  </p>
-                )}
-                {!!result.similarTo && (
-                  <p>
-                    <strong>similar to</strong>
-                    <p>{result.similarTo.join(', ')}</p>
-                  </p>
-                )}
-                {!!result.examples && (
-                  <p>
-                    <strong>examples</strong>
-                    <p>{result.examples.join(', ')}</p>
-                  </p>
-                )}
-                <hr />
-              </div>
-            ))
-          ) : (
-            <em>
-              <br />The definition for <em>{data.word}</em> is not available at this time
-            </em>
+          {!!data.syllables && (
+            <h2>
+              {// Syllables
+              data.syllables.list.join('-')}
+            </h2>
           )}
+          {!!data.pronunciation &&
+            !!data.pronunciation.all && (
+              <p className="font-italic pronunciation">
+                {// Pronunciation
+                '\\' + data.pronunciation.all + '\\'}
+              </p>
+            )}
+          <div>
+            {!!data.results ? (
+              data.results.map(result => (
+                <div>
+                  <p className="font-italic partOfSpeech">
+                    {result.partOfSpeech}
+                  </p>
+                  <p>{': ' + result.definition}</p>
+                  {!!result.synonyms && (
+                    <p>
+                      <strong>synonyms</strong>
+                      <p>{result.synonyms.join(', ')}</p>
+                    </p>
+                  )}
+                  {!!result.similarTo && (
+                    <p>
+                      <strong>similar to</strong>
+                      <p>{result.similarTo.join(', ')}</p>
+                    </p>
+                  )}
+                  {!!result.examples && (
+                    <p>
+                      <strong>examples</strong>
+                      <p>{result.examples.join(', ')}</p>
+                    </p>
+                  )}
+                  <hr />
+                </div>
+              ))
+            ) : (
+              <em>
+                <br />The definition for <em>{data.word}</em> is not available
+                at this time
+              </em>
+            )}
+          </div>
         </div>
-      </div>
-        ) : (
-          <h4>
-            <br />Hello, this is <em>Eloquent Dictionary</em>.<br/>Begin by searching a <strong>word</strong>
-          </h4>
-        )
+      ) : !!word ? (
+        <h4 className="responsiveHello">
+          <br />You are typing...<br />
+          Press the <strong>Enter</strong> key
+          <br />when you are done
+        </h4>
       ) : (
+        <h4 className="responsiveHello">
+          <br />Hello, this is the<br />
+          <em>Eloquent Dictionary</em>.<br />Begin by searching a{' '}
+          <strong>word</strong>
+        </h4>
+      )
+    ) : (
       <em>
-        <br />The word you are searching for cannot be found in this dictionary
+        <br />Unfortunately, the word you are searching for cannot be found in
+        this dictionary
       </em>
     )}
   </div>
